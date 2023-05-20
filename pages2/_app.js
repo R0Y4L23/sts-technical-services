aa22e7eb2c3343f9caed12331c1dcf383ad0e43f
Subham Roy <subhamroy102.hitece2020@gmail.com>
2023-05-20 16:37:36 +0530

    Maintenance

--- pages2/_app.js
+++ pages2/_app.js
@@ -4,5 +4,5 @@ import Header from '@/components/header'
 import Footer from '@/components/footer'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <><Header /><div className="min-h-[61vh] "><Component {...pageProps} /></div><Footer /></>
 }
